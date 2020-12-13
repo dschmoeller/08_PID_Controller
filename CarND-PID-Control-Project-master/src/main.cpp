2d10430ca2b99ec787a6257bee6d79fe70145d35
dschmoeller <dominikschmoeller@gmail.com>
2020-12-13 13:54:40 +0100

    Parameter manual tweaking

--- CarND-PID-Control-Project-master/src/main.cpp
+++ CarND-PID-Control-Project-master/src/main.cpp
@@ -30,17 +30,23 @@ string hasData(string s) {
   return "";
 }
 
+// Variables for Optimization Procedure
+int cnt = 0;
+int checkpoint_length = 300;
+double last_checkpoint_error = 0;   
+
 int main() {
   uWS::Hub h;
 
   // Define PID controller
   PID pid = PID(); 
-  double Kp = 0.1; 
+  double Kp = 0.13; 
   double Ki = 0.001; 
-  double Kd = 0.5;  
+  double Kd = 1.22;  
   // Initialize PID control parameters
   pid.Init(Kp, Ki, Kd); 
 
+  // Simulator Interface 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, 
                      uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -68,15 +74,23 @@ int main() {
           if (steer_value > 1){ steer_value = 1; }
           else if (steer_value < -1){ steer_value = -1; }   
 
+          // Optimization Procedure
+          /*cnt ++;
+          if (cnt > checkpoint_length){
+            double checkpoint_error = pid.TotalError() - last_checkpoint_error; 
+            std::cout << "C H E C K P O I N T   --> Error is: " << checkpoint_error << std::endl; 
+            cnt = 0; 
+            last_checkpoint_error = pid.TotalError(); 
+          }*/
+
           // DEBUG
-          std::cout << "CTE: " << cte << " Steering Value: " << steer_value 
-                    << std::endl;
+          //std::cout << "CTE: " << cte << std::endl;
 
           json msgJson;
           msgJson["steering_angle"] = steer_value;
           msgJson["throttle"] = 0.3;
           auto msg = "42[\"steer\"," + msgJson.dump() + "]";
-          std::cout << msg << std::endl;
+          //std::cout << msg << std::endl;
           ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
         }  // end "telemetry" if
       } else {
